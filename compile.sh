0114649dcadcefaf51b9c1bf8fc5399ecbfbc196
Collin Brohm <collin.brohm@midaspeakcap.com>
2024-11-14 14:10:54 -0500

    fixed compile script

--- compile.sh
+++ compile.sh
@@ -3,11 +3,5 @@ rm -rf bin
 
 
 
-javac -d bin -cp src:test test/TestTupleGenerator.java
-javac -d bin -cp src:test test/TupleGeneratorImpl.java
-javac -d bin -cp src:test test/TupleGenerator.java
-javac -d bin src/ArrayUtil.java
-javac -d bin src/KeyType.java  
-javac -d bin src/LinHashMap.java 
-javac -d bin src/Table.java
+javac -d bin -cp src src/*.java 
 
