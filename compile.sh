5dc75b9d1c183611624e0261e0ce069d2bd991c6
Collin Brohm <collin.brohm@midaspeakcap.com>
2024-11-14 13:32:23 -0500

    fixed again2

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -ex
+!/bin/bash -ex
 rm -rf bin
 
 
-# Fix file names and add proper sourcing
+
 javac -d bin -cp src:test test/TestTupleGenerator.java
 javac -d bin -cp src:test test/TupleGeneratorImpl.java
 javac -d bin -cp src:test test/TupleGenerator.java
