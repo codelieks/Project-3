45ecc7e0817be11939257b0df8a009f2dbc72629
Collin Brohm <collin.brohm@midaspeakcap.com>
2024-11-14 13:31:56 -0500

    fixed again

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ javac -d bin -cp src:test test/TestTupleGenerator.java
 javac -d bin -cp src:test test/TupleGeneratorImpl.java
 javac -d bin -cp src:test test/TupleGenerator.java
 javac -d bin src/ArrayUtil.java
-javac -d bin src/KeyType.java  # Fixed typo in KeyType
-javac -d bin src/LinHashMap.java  # Fixed typo in LinHashMap
+javac -d bin src/KeyType.java  
+javac -d bin src/LinHashMap.java 
 javac -d bin src/Table.java
 
